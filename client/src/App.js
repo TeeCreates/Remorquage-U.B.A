808b67ff9a7db804ead6640c881462d62d6fab1b
TeeCreates <99111124+TeeCreates@users.noreply.github.com>
2024-08-25 14:55:42 -0400

    test

--- client/src/App.js
+++ client/src/App.js
@@ -4,18 +4,21 @@ import Contact from "./pages/Contact"
 import Services from "./pages/Services"
 import About from "./pages/About"
 import Home from "./Home"
+import {MainContent} from "./MainContent"
+
 function App() {
   return (
 
       <Router>
         <Navbar/>
-
+        <MainContent>
       <Switch>
         <Route exact path="/" component={Home} />
         <Route path="/services" component={Services} />
         <Route path="/about" component={About} />
         <Route path="/contact" component={Contact} />
       </Switch>
+      </MainContent>
     </Router>
   );
 }
---
+++ client/src/MainContent.js
@@ -0,0 +1,5 @@
+import styled from "styled-components"
+
+export const MainContent = styled.div`
+  margin-top: 60px; // Add top margin to account for fixed navbar
+`;
